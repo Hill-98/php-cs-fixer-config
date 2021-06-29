6f982f9903b74f5340e6f0451ca9a2a1ecd7fc0e
Zhong Lufan <lufanzhong@gmail.com>
2021-06-29 18:21:46 +0800

    Minor change

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -4,7 +4,6 @@ define('RISKY', true); // Allow Risky rules
 
 $finder = PhpCsFixer\Finder::create()->in(__DIR__)->exclude('rules');
 
-
 /** @var \PhpCsFixer\Config $config */
 $config = require __DIR__.'/main.php';
 
